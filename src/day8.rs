5677e48281078515d158c30ae6ca85542d2f9f97
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-12-09 21:27:05 +0000

    did d8p2

--- src/day8.rs
+++ src/day8.rs
@@ -29,7 +29,7 @@ pub fn d8(text : Vec<&str>) -> i32 {
     return count;
 }
 
-pub fn d8p2(text : Vec<&str>) -> i32 {
+pub fn d8p2OLD(text : Vec<&str>) -> i32 {
     let path = text[0];
 
     let mut mapping : Vec<(usize, usize)> = vec![(18000, 18000); 17576];
@@ -82,4 +82,67 @@ pub fn d8p2(text : Vec<&str>) -> i32 {
 
     return  count;
 
+}
+
+pub fn d8p2(text : Vec<&str>) -> i64 {
+    let path = text[0];
+
+    let mut mapping : Vec<(usize, usize)> = vec![(18000, 18000); 17576];
+    let mut starts : Vec<usize> = Vec::new();
+    for l in text[2..].iter() {
+        let (node, dirs) = l.split_once(" = ").unwrap();
+        let dchars : Vec<char> = dirs.chars().collect();
+        let left = (dchars[1] as usize - 65) * 676 + (dchars[2] as usize - 65) * 26 + (dchars[3] as usize - 65);
+        let right = (dchars[6] as usize - 65) * 676 + (dchars[7] as usize - 65) * 26 + (dchars[8] as usize - 65);
+        let nodeNum = (node.chars().nth(0).unwrap() as usize - 65) * 676
+            + (node.chars().nth(1).unwrap() as usize - 65) * 26
+            + node.chars().nth(2).unwrap() as usize - 65;
+        //println!("{}-{}",node, dirs);
+        //println!("{}+{}+{}", (node.chars().nth(0).unwrap() as usize * 676), (node.chars().nth(1).unwrap() as usize * 26), )
+        mapping[nodeNum] =  (left, right);
+
+        if (node.chars().nth(2).unwrap() == 'A') {
+            starts.push(nodeNum)
+        }
+    }
+
+    //println!("PArse donw");
+    
+    //mapping.iter().for_each(|k| if k % 32 == 1 { current.push(*k)});
+    //(0..17576).step_by(26).for_each(|n| if mapping[n].0 < 18000 { current.push(n)});
+    //println!("{} seeds", starts.len());
+    
+    let chars : Vec<char> = path.chars().collect();
+    starts.iter().map(|node| {
+        let mut count = 0;
+    let mut index : usize = 0;
+        let mut current = node;
+        while (current % 26 < 25) {
+            current = match chars[index] {
+                    'L' => &mapping[*current].0,
+                    'R' => &mapping[*current].1,
+                    _ => unreachable!()
+                };
+            count += 1;
+            index += 1;
+            
+            index = index % path.len();
+        }
+        //println!("{}", count);
+        //println!("{}-{}-{}", current, count, index);
+    count
+    }).reduce(lcm).unwrap()
+
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    (a * b) / gcd(a, b)
 }
