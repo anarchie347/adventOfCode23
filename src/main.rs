cdbfe5e94fc30f025af869a75705c481f218d71c
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-12-09 19:32:29 +0000

    added benchamark

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod day8;
 mod day9;
 
 fn main() {
-    run(day9::d9p2, 9);
+    bench(day9::d9p2, 9);
     //test(day5::d5p2, 5);
 }
 
@@ -30,4 +30,16 @@ fn test(f : fn(Vec<&str>) -> i32, _day : usize) {
     let splitText = text.split("\n").map(|s| {s.trim()}).collect();
     let result = f(splitText);
     println!("TestResult: {}", result);
+}
+
+fn bench(f : fn(Vec<&str>) -> i32, day : usize) {
+    let text = std::fs::read_to_string(format!("src/inputs/{}.txt", day)).expect("couldnt read file");
+    let splitText : Vec<&str> = text.split("\n").map(|s| {s.trim()}).collect();
+    const repetitions : u32= 1000;
+    let instant = std::time::Instant::now();
+    for i in 0..repetitions {
+        f(splitText.clone());
+    }
+    
+    println!("Average time: {:?}", instant.elapsed() / repetitions);
 }
